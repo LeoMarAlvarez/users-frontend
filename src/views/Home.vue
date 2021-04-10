9acfc9aa51eab296fcee362d28e2b4a7a0ea0f8b
LEO <alvarezleom@gmail.com>
2021-04-10 01:58:30 -0300

    Installation package Axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,12 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+// import axios from 'axios';
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
-  },
+  }
 };
 </script>
